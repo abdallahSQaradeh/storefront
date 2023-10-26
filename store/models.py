a3fd47a6162b6aa74229e2db0b5a618e4b43710c
abdallahsqaradeh <abdallahsqaradeh@gmail.com>
2023-10-26 09:49:32 +0200

    setting up postgress and fix the customer email type

--- store/models.py
+++ store/models.py
@@ -42,7 +42,7 @@ class Customer(models.Model):
     ]
     first_name = models.CharField(max_length=255)
     last_name= models.CharField(max_length=255)
-    email = models.DateField(unique=True)
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateTimeField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
