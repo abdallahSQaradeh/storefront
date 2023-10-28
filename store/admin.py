00b7e805711136a27bcec35cd186609d7a8905b4
abdallahsqaradeh <abdallahsqaradeh@gmail.com>
2023-10-28 16:34:07 +0200

    Customize the Admin site for collections, products , orders and customers

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,60 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.db.models import Count
+from . import models
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'unit_price',
+        'inventory_status',
+        'collection' # will display the string repesentation
+        # to get a specific property from the related model,
+        #  we can define a method. .g collection_title
+        ,'collection_title'
+        ]
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection'] # this to enhance the query and prevent django from 
+    # having a query for each product
+    
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'low'
+        return 'ok'
+    
+    def collection_title(self, product):
+        return product.collection.title
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','membership']
+    list_editable = ['membership']
+    ordering = ['first_name','last_name']
+    list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','customer']
+    
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title','products_count']
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        return collection.products_count
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(products_count = Count('product') )
+
+
 
 # Register your models here.
+#admin.site.register(models.Collection)
+# admin.site.register(models.Product)
--- store/models.py
+++ store/models.py
@@ -12,6 +12,11 @@ class Collection(models.Model):
     class, to work around it we can specify the related name to be "+" to prevent django from creating the
     reverse relationship
     '''
+    def __str__(self):
+        return self.title + f'- {self.id}'
+    
+    class Meta:
+        ordering =['title']
     
 class Promotion(models.Model): 
     description = models.CharField(max_length=255)
@@ -29,6 +34,12 @@ class Product(models.Model):
     promotions = models.ManyToManyField(Promotion
                                         #, related_name="productts"
                                         )
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE='B'
@@ -53,6 +64,12 @@ class Customer(models.Model):
         db_table = "store_customers"
         indexes = [models.Index(fields=['last_name','first_name'])]
     '''    
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name','last_name']
 
 
 class Order(models.Model):
