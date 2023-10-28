dc47da1769575402faf3ff4068871af046bbe02b
abdallahsqaradeh <abdallahsqaradeh@gmail.com>
2023-10-28 15:40:48 +0200

    add custum manager to taggeditems

--- tags/models.py
+++ tags/models.py
@@ -2,11 +2,23 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+class TaggedItemMnager(models.Manager):
+    def get_tags_for(self,obj_type, obj_id ):
+        # this will get the content type id for the Product model from the django_contetnt_type
+        content_type = ContentType.objects.get_for_model(obj_type)
+        queryset = TaggedItem.objects\
+            .select_related('tag')\
+            .filter(content_type=content_type,
+                object_id = obj_id # product id whose tags we want to query 
+                ) 
+        return queryset
+
 # Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 class TaggedItem(models.Model):
+    objects = TaggedItemMnager()
     # What tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     '''
