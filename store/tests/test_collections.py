05ed8a97c1ff38ff1892cdae98660b39d0ddb2d7
abdallahsqaradeh <abdallahsqaradeh@gmail.com>
2023-11-24 07:02:11 +0200

    add authentication test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -13,3 +13,10 @@ class TestCreateCollection:
 
         # Assert
         assert respnse.status_code  == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        respnse = client.post('/store/collections/',{'title':'a'})
+
+        assert respnse.status_code  == status.HTTP_403_FORBIDDEN
