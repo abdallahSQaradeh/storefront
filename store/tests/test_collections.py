312400f5ca069af4aadaf400895c6a9265e657b8
abdallahsqaradeh <abdallahsqaradeh@gmail.com>
2023-11-24 07:44:00 +0200

    add multiple assertions

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -21,3 +21,20 @@ class TestCreateCollection:
         respnse = client.post('/store/collections/',{'title':'a'})
 
         assert respnse.status_code  == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_invalid_returns_400(self):
+            client = APIClient()
+            client.force_authenticate(user=User(is_staff = True))
+            respnse = client.post('/store/collections/',{'title':''})
+
+            # TESTS     should test single thing
+            assert respnse.status_code  == status.HTTP_400_BAD_REQUEST
+            assert respnse.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff = True))
+        respnse = client.post('/store/collections/',{'title':'a'})
+
+        assert respnse.status_code  == status.HTTP_201_CREATED
+        assert respnse.data['id'] > 0
