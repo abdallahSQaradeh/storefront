679428140a8fe86224120efb44b024615ea1d404
abdallahSQaradeh <abdallahsqaradeh@gmailcom>
2023-11-25 21:08:45 +0200

    add home page, and setup the serving for statics in the development server

--- storefront/urls.py
+++ storefront/urls.py
@@ -19,10 +19,12 @@ from django.conf.urls.static import static
 from django.urls import path, include
 import debug_toolbar
 
+
 admin.site.site_header = "Storefront admin"
 admin.site.index_title ="Admin"
 
 urlpatterns = [
+    path("",include("core.urls")),
     path('admin/', admin.site.urls),
     path("playground/",include("playground.urls")),
     path("store/", include("store.urls")),
